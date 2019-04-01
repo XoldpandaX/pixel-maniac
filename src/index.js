7d249853574f90cdb51829676e08f30dc517a598
XoldpandaX <rembo-87@yandex.ru>
2019-04-01 18:18:17 +0700

    added relative path

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-import App from './components/app';
-import './index.scss';
+import App from 'components/app';
+import 'index.scss';
 
 import * as reducers from './store'
+
+import { createFontAwesomeIcons } from 'utils';
+createFontAwesomeIcons();
 
 const store = createStore(
   combineReducers(reducers),
