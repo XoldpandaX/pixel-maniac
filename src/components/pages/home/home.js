12e26ac04cbec6eb1ef03261bf7bed22afc24dc3
XoldpandaX <rembo-87@yandex.ru>
2019-04-03 22:27:19 +0700

    added homepage component && implement base template

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import AuthorizationFormContainer from 'components/template-containers/authorization-form';
 import AppInput from 'components/controls/app-input';
+
+import styles from './home.module.scss';
 
 class HomePage extends Component {
   state = {
@@ -12,7 +15,7 @@ class HomePage extends Component {
   appInputChildren = (
     <FontAwesomeIcon
       icon='user'
-      size='md'
+      size='sm'
       color='white'
     />
   ); // stop re-render
@@ -35,20 +38,38 @@ class HomePage extends Component {
   
   
   render() {
+    const {
+      homePage,
+      authorizationCol,
+      widgetCol
+    } = styles;
+    
     return (
-      <div>
-        <button onClick={ this.showError }>show error</button>
-        <button onClick={ this.hideError }>hide error</button>
-        <p>{ this.state.inputField }</p>
-        <AppInput
-          handleChange={ this.onInputChange }
-          handleBlur={ this.onInputBlur }
-          placeholder='User Name'
-          hasError={ this.state.hasError }
-          errorText='Error mfcka'
-        >
-          { this.appInputChildren }
-        </AppInput>
+      <div className={ homePage }>
+        <section className={ widgetCol }>
+          <p>WIDGEET SIDE</p>
+        </section>
+        <section className={ authorizationCol }>
+          <AuthorizationFormContainer
+            type='login'
+            title='Login'
+          >
+            <>
+              <button onClick={ this.showError }>show error</button>
+              <button onClick={ this.hideError }>hide error</button>
+              <p>{ this.state.inputField }</p>
+              <AppInput
+                handleChange={ this.onInputChange }
+                handleBlur={ this.onInputBlur }
+                placeholder='User Name'
+                hasError={ this.state.hasError }
+                errorText='Error mfcka'
+              >
+                { this.appInputChildren }
+              </AppInput>
+            </>
+          </AuthorizationFormContainer>
+        </section>
       </div>
     );
   }
