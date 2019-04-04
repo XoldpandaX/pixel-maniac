f82b358197fa9795aab1e7c61f0e530e10f4c7bb
XoldpandaX <rembo-87@yandex.ru>
2019-04-04 20:37:32 +0700

    added new title component

---
+++ src/components/app-title/app-title.js
@@ -0,0 +1,49 @@
+import React, { createElement } from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './app-title.module.scss';
+
+const AppTitle = (props) => {
+  const {
+    level,
+    children,
+    classNames,
+    color,
+    hasTextShadow
+  }= props;
+  
+  const { appTitle, textShadow } = styles;
+  
+  const textShadowClass = hasTextShadow ? textShadow : null;
+  
+  const availableLevels = {
+    h1: 'h1',
+    h2: 'h2',
+    h3: 'h3'
+  };
+  
+  return createElement(
+    `${ availableLevels[level] }`,
+    {
+      className: `${ appTitle } ${ textShadowClass } ${ classNames }`,
+      style: { color },
+    },
+    children
+  );
+};
+
+AppTitle.defaultProps = {
+  classNames: '',
+  color: '#fff',
+  hasTextShadow: true
+};
+
+AppTitle.propTypes = {
+  level: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+  classNames: PropTypes.string,
+  color: PropTypes.string,
+  hasTextShadow: PropTypes.bool
+};
+
+export default AppTitle;
