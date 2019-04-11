431ac9fe9ead6af65d70d1b0862c921a49c9af3d
XoldpandaX <rembo-87@yandex.ru>
2019-04-11 22:30:15 +0700

    added new action && add test request

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,8 +1,16 @@
 import * as types from './action-types';
+import Firebase from 'services/firebase';
 
-function register(userData) {
+function register(userRegInfo) {
   return async(dispatch) => {
-    console.info(userData);
+    try {
+      const { userEmail, userPassword } = userRegInfo;
+      const data = await Firebase.call('registration', userEmail, userPassword);
+      console.info(data);
+      console.info('sdfsdf');
+    } catch (e) {
+      console.error('action: register', e);
+    }
   };
 }
 
