23343b399e50f2bdf82829ab92f59e6b0544a698
XoldpandaX <rembo-87@yandex.ru>
2019-04-12 20:07:49 +0700

    added spinner && add it logic && types

--- src/components/common/spinner/spinner.js
+++ src/components/common/spinner/spinner.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './spinner.module.scss';
 
-const Spinner = (props) => {
-  const {
-    loader,
-    loaderSizeXL,
-    loaderSizeM,
-    loaderSizeL,
-    loaderSizeS
-  } = styles;
-  // const sizes = {
-  //   m: styles.sizeS,
-  // };
+const Spinner = ({ size, className }) => {
+  const { loader } = styles;
+  const sizes = {
+    s: styles.loaderSizeS,
+    m: styles.loaderSizeM,
+    l: styles.loaderSizeL,
+    xl: styles.loaderSizeXL,
+  };
+  
+  const classNames = className
+    ? `${ loader } ${ sizes[size] }`
+    : `${ className } ${ loader } ${ sizes[size] }`;
+  
   return (
-    <div className={ `${ loader } ${ loaderSizeS }` } />
+    <div className={ classNames } />
   );
+};
+
+Spinner.defaultProps = {
+  className: ''
+};
+
+Spinner.propTypes = {
+  size: PropTypes.oneOf(['s', 'm', 'l', 'xl']).isRequired
 };
 
 export default Spinner;
