b6f0078e983b5a6492f5631970b2da3f083a9bff
XoldpandaX <rembo-87@yandex.ru>
2019-04-16 20:44:38 +0700

    added animate pre-loader

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,64 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
+import GlobalLoader from 'components/common/global-loader';
 import HomePage from 'components/pages/home';
 
 import styles from './app.module.scss';
 
 class App extends Component {
+  state = {
+    isAppRender: false,
+    timeout: 2000
+  };
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isAppRender: true })
+    }, this.state.timeout);
+  }
   
   render () {
-    const {
-      app
-    } = styles;
+    const { isAppRender } = this.state;
+    const { app } = styles;
     
+    const loader = !isAppRender &&
+      <CSSTransition
+        classNames='a-slide'
+        in={ !isAppRender }
+        timeout={{
+          enter: 400,
+          exit: 250
+        }}
+        unmountOnExit
+      >
+        <GlobalLoader />
+      </CSSTransition>;
+  
     return (
-      <div className={ app }>
-        <main style={ { padding: '50px 50px 2px' } }>
-          <header>this is a header</header>
-          <Switch>
-            <Route
-              path='/'
-              component={ HomePage }
-              exact
-            />
-          </Switch>
-          <footer>this is a footer</footer>
-        </main>
+      <div className={app}>
+        <CSSTransition
+          classNames='a-slide'
+          in={ isAppRender }
+          timeout={{ enter: 400 }}
+          unmountOnExit
+        >
+          <main style={{padding: '50px 50px 2px'}}>
+            <div>
+              <header>this is a header</header>
+              <Switch>
+                <Route
+                  path='/'
+                  component={HomePage}
+                  exact
+                />
+              </Switch>
+              <footer>this is a footer</footer>
+            </div>
+          </main>
+        </CSSTransition>
+        { loader }
       </div>
     );
   }
