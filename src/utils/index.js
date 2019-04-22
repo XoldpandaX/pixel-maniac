28dc3ffe03077e0c643b8558139a7a6d89c90afb
XoldpandaX <rembo-87@yandex.ru>
2019-04-22 22:36:56 +0700

    small fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
 import collectionKeysToCamelCase from './collection-keys-to-camel-case';
 import createFontAwesomeIcons from './font-awesome';
-import firebaseInit from './firebase-initializer';
 import objKeysToSnakeCase from './obj-keys-to-snake-case';
 import requestImage from './request-image';
 import showNotification from './notification';
@@ -10,7 +9,6 @@ import storeCreator from './store-creator';
 export {
   collectionKeysToCamelCase,
   createFontAwesomeIcons,
-  firebaseInit,
   objKeysToSnakeCase,
   requestImage,
   showNotification,
