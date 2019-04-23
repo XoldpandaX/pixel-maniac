f23e3579fdbadc5667ab9e2bc3a99f57c956bc5c
XoldpandaX <rembo-87@yandex.ru>
2019-04-23 09:12:18 +0700

    edit config js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const DISABLE_CORS_URI = 'https://cors-anywhere.herokuapp.com/';
-
 const IMAGE_SERVER_URI = 'https://wall.alphacoders.com/api2.0/get.php';
-
 const IMAGE_SERVER_ENDPOINT = `${ DISABLE_CORS_URI }${ IMAGE_SERVER_URI }`;
 
 const NOTIFICATION_TIMEOUT = 4000;
