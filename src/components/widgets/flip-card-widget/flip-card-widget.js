e440521feaf073030f7b714e50a9b506c68ae171
XoldpandaX <rembo-87@yandex.ru>
2019-04-23 22:55:02 +0700

    small additions

--- src/components/widgets/flip-card-widget/flip-card-widget.js
+++ src/components/widgets/flip-card-widget/flip-card-widget.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import random from 'lodash/random';
 
 import FlipCard from 'components/common/flip-card';
 
@@ -7,25 +8,28 @@ import styles from './flip-card-widget.module.scss';
 
 class FlipCardWidget extends Component {
   state = {
-    counter: 0
+    counter: 0,
+    row: null
   };
   
   componentDidMount() {
     this.timerId = setTimeout(function tick() {
-      this.setState({ counter: this.state.counter += 1 });
-      this.timerId = setTimeout(tick.bind(this), 2000);
-    }.bind(this), 2000);
+      const rand = random(0, 4);
+      this.setState({ row: rand });
+      
+      this.timerId = setTimeout(tick.bind(this), 1000);
+    }.bind(this), 1000);
   }
   
-  stopTimer = () => {
+  componentWillUnmount() {
     clearTimeout(this.timerId);
-  };
+  }
   
   render() {
     return (
       <div>
         <button onClick={ this.stopTimer }>stop timer</button>
-        <div>{ this.state.counter }</div>
+        <div>{ this.state.row }</div>
         <FlipCard
           frontImage='https://images7.alphacoders.com/548/thumb-548306.jpg'
           backImage='https://images5.alphacoders.com/697/thumb-697989.jpg'
