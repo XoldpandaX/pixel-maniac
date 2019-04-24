6219ff711c11d8354e35789e4a78a74452f3d0b0
XoldpandaX <rembo-87@yandex.ru>
2019-04-24 23:29:07 +0700

    widget component for flip card list

--- src/components/widgets/flip-card-widget/flip-card-widget.js
+++ src/components/widgets/flip-card-widget/flip-card-widget.js
@@ -1,44 +1,31 @@
-import React, {Component} from 'react';
+import React  from 'react';
 import PropTypes from 'prop-types';
-import random from 'lodash/random';
 
 import FlipCard from 'components/common/flip-card';
 
 import styles from './flip-card-widget.module.scss';
 
-class FlipCardWidget extends Component {
-  state = {
-    counter: 0,
-    row: null
-  };
-  
-  componentDidMount() {
-    this.timerId = setTimeout(function tick() {
-      const rand = random(0, 4);
-      this.setState({ row: rand });
-      
-      this.timerId = setTimeout(tick.bind(this), 1000);
-    }.bind(this), 1000);
-  }
-  
-  componentWillUnmount() {
-    clearTimeout(this.timerId);
-  }
-  
-  render() {
-    return (
-      <div>
-        <button onClick={ this.stopTimer }>stop timer</button>
-        <div>{ this.state.row }</div>
-        <FlipCard
-          frontImage='https://images7.alphacoders.com/548/thumb-548306.jpg'
-          backImage='https://images5.alphacoders.com/697/thumb-697989.jpg'
-          isFlip={ true }
-        />
-      </div>
-    );
-  }
-}
+const FlipCardWidget = ({ images }) => {
+  return (
+    <div style={{display: 'flex' }}>
+      {
+        images.map(({ id, front, back, isFlip, hasVerticalFlip }) => (
+          <div key={ id }>
+            <FlipCard
+              id={ id }
+              frontImage={ front.urlThumb }
+              backImage={ back.urlThumb }
+              isFlip={ isFlip }
+              hasVerticalFlip={ hasVerticalFlip }
+              width='245px'
+              height='163px'
+            />
+          </div>
+        ))
+      }
+    </div>
+  );
+};
 
 FlipCardWidget.propTypes = {};
 
