14ebb822bb6fcb0de5afadb39946f6d83df275e0
XoldpandaX <rembo-87@yandex.ru>
2019-04-24 09:10:26 +0700

    update mapper

--- src/store/images/mappers.js
+++ src/store/images/mappers.js
@@ -3,11 +3,17 @@ import { collectionKeysToCamelCase } from 'utils';
 
 
 function highestRatedImageMapper(images) {
-  const mapped =  images
-    .filter((image, idx) => idx <= 37)
+  const mappedImages = images
+    .filter((image, idx) => idx <= 45)
     .map((image) => pick(image, ['id', 'url_thumb']));
   
-  return collectionKeysToCamelCase(mapped);
+  const camelCasedImages = collectionKeysToCamelCase(mappedImages);
+  return {
+    flipSlider: camelCasedImages.splice(0, 8),
+    classicSlider: camelCasedImages.splice(0, 14),
+    singleVerticalSlider: camelCasedImages.splice(0, 9),
+    singleHorizontalSlider: [...camelCasedImages]
+  };
 }
 
 export {
